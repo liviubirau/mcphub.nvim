792359a19090eadab76335fedf2f04de59a8d23c
Titus BIRAU <titus.birau@intel.com>
2025-06-16 14:04:16 +0300

    lualine extension updated

--- lua/mcphub/extensions/lualine.lua
+++ lua/mcphub/extensions/lualine.lua
@@ -124,13 +124,15 @@ end
 -- Update function that lualine calls
 function M:update_status()
     -- Get status display
-    local status_icon, status_hl = self:get_status_display()
+    -- local status_icon, status_hl = self:get_status_display()
 
     -- Show either the spinner or the number of active servers
     local count_or_spinner = vim.g.mcphub_executing and spinner_frames[current_frame]
         or tostring(vim.g.mcphub_active_servers or 0)
     -- Format the status line
-    return self:format_hl(status_icon .. " " .. count_or_spinner .. " ", status_hl)
+    -- return self:format_hl(status_icon .. " " .. count_or_spinner .. " ", status_hl)
+    local tower = "󰐻"
+    return tower .. " " .. count_or_spinner
 end
 
 -- Cleanup
